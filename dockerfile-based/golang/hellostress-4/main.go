beb4ad9674bf1360e8526cd62e0bd593ecc7aa91
Matthias Diester <matthias.diester@de.ibm.com>
2020-11-03 10:43:06 +0100

    Introduce two stress builds
    Rename current stress build to `hellostress-2` to indicate the two CPUs being used.
    
    Add `hellostress-4` with four CPUs used in the build.

---
+++ dockerfile-based/golang/hellostress-4/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	port := 8080
+	if strValue, ok := os.LookupEnv("PORT"); ok {
+		if intValue, err := strconv.Atoi(strValue); err == nil {
+			port = intValue
+		}
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, Homeport!")
+	})
+
+	fmt.Printf("Starting listener on port %d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
